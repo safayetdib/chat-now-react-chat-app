094ac7c6562e3061f62382dad3098a5fbcceaa93
safayet <safayetdib@gmail.com>
2023-01-13 23:58:42 +0600

    chat-engine added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,36 +1,94 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
+import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 
+import { useAuth } from '../contexts/AuthContext';
+
 import { auth } from '../firebase';
 
-const Chats = () => {
+export default function Chats() {
+	const didMountRef = useRef(false);
+	const [loading, setLoading] = useState(true);
+	const { user } = useAuth();
 	const history = useHistory();
 
-	const handleLogout = async () => {
+	async function handleLogout() {
 		await auth.signOut();
-
 		history.push('/');
-	};
+	}
+
+	async function getFile(url) {
+		let response = await fetch(url);
+		let data = await response.blob();
+		return new File([data], 'test.jpg' || 'test.png', {
+			type: 'image/jpeg' || 'image/png',
+		});
+	}
+
+	useEffect(() => {
+		if (!didMountRef.current) {
+			didMountRef.current = true;
+
+			if (!user || user === null) {
+				history.push('/');
+				return;
+			}
+
+			// Get-or-Create should be in a Firebase Function
+			axios
+				.get('https://api.chatengine.io/users/me/', {
+					headers: {
+						'project-id': process.env.REACT_APP_CHAT_ENGINE_ID,
+						'user-name': user.email,
+						'user-secret': user.uid,
+					},
+				})
+
+				.then(() => setLoading(false))
+
+				.catch((e) => {
+					let formdata = new FormData();
+					formdata.append('email', user.email);
+					formdata.append('username', user.email);
+					formdata.append('secret', user.uid);
+
+					getFile(user.photoURL).then((avatar) => {
+						formdata.append('avatar', avatar, avatar.name);
+
+						axios
+							.post('https://api.chatengine.io/users/', formdata, {
+								headers: {
+									'private-key': process.env.REACT_APP_CHAT_ENGINE_KEY,
+								},
+							})
+							.then(() => setLoading(false))
+							.catch((e) => console.log('e', e.response));
+					});
+				});
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		}
+	}, [user, history]);
+
+	if (!user || loading) return <div />;
 
 	return (
 		<div className="chats-page">
 			<div className="nav-bar">
 				<div className="logo-tab">ChatNow</div>
-				<div className="logout-tab" onClick={handleLogout}>
+
+				<div onClick={handleLogout} className="logout-tab">
 					Logout
 				</div>
 			</div>
-			{/* project-id===> 7f40b101-8b50-49db-9ba6-c7dcdd9c9fff */}
+
 			<ChatEngine
 				height="calc(100vh - 66px)"
-				projectId="7f40b101-8b50-49db-9ba6-c7dcdd9c9fff"
-				userName="."
-				userSecret="."
+				projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+				userName={user.email}
+				userSecret={user.uid}
 			/>
 		</div>
 	);
-};
-
-export default Chats;
+}
